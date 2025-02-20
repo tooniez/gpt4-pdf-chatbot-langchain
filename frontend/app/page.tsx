4bb98092472d0af57db600a10ba2183d76adecc4
Mayo Oshin <107035552+mayooear@users.noreply.github.com>
2025-02-20 18:19:57 +0000

    Update page.tsx
    Update book url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -275,7 +275,7 @@ export default function Home() {
               <p className="font-medium text-muted-foreground max-w-md mx-auto">
                 This ai chatbot is an example template to accompany the book:{' '}
                 <a
-                  href="https://tinyurl.com/learning-langchain"
+                  href="https://www.oreilly.com/library/view/learning-langchain/9781098167271/"
                   className="underline hover:text-foreground"
                 >
                   Learning LangChain (O'Reilly): Building AI and LLM
